7705ff0090967120f1bc3de4f3ae761854b78236
Isaac Freeman <isaac@resolvedigital.co.nz>
2016-06-07 16:27:23 +1200

    Use existing stock prioritizer first
    Calls the existing Spree::Stock::Prioritizer#sort_packages before moving reserved item locations to the front.
    
    This should preserve any prioritization of the other stock locations.

--- app/models/spree/stock/prioritizer_decorator.rb
+++ app/models/spree/stock/prioritizer_decorator.rb
@@ -1,8 +1,11 @@
 Spree::Stock::Prioritizer.class_eval do
   private
 
-  # Overridden to use reserved_stock_location first
+  # Use reserved_stock_location first
+  alias_method :original_sort_packages, :sort_packages
   def sort_packages
+    original_sort_packages
+
     reserved_stock_location = Spree::StockLocation.reserved_items_location
     @packages = @packages
                 .partition do |package|
