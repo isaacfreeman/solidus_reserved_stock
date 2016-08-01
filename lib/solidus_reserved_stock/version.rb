6b5afddddf6da7b6dd31208d7d1b41368b35a9ce
Isaac Freeman <isaac@resolvedigital.co.nz>
2016-08-01 14:24:20 +1200

    Version bump

--- lib/solidus_reserved_stock/version.rb
+++ lib/solidus_reserved_stock/version.rb
@@ -10,7 +10,7 @@ module SolidusReservedStock
   module VERSION
     MAJOR = 0
     MINOR = 2
-    TINY  = 4
+    TINY  = 5
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
