6317b6bbe2121c859ec2e374aca16823e57e9f5b
Isaac Freeman <isaac@resolvedigital.co.nz>
2016-08-02 14:33:58 +1200

    Refactoring

--- app/models/spree/stock/reserver.rb
+++ app/models/spree/stock/reserver.rb
@@ -2,9 +2,11 @@ module Spree
   module Stock
     # Reserve and restock variants
     class Reserver
+      # Error to return if any requested quantity is impossible
       class InvalidQuantityError < StandardError; end
 
-      def initialize(variant = nil, user = nil, original_stock_location = nil, reserved_stock_location = nil)
+      def initialize(variant = nil, user = nil, original_stock_location = nil,
+                     reserved_stock_location = nil)
         @variant = variant
         @user = user
         @original_stock_location = original_stock_location
@@ -18,12 +20,7 @@ module Spree
       # TODO: Raise suitable error if variant is nil (otherwise we get
       #       InvalidQuantityError)
       def reserve(quantity, expires_at = nil)
-        if quantity < 1 || quantity.blank?
-          raise InvalidQuantityError, Spree.t(:quantity_must_be_positive)
-        end
-        if quantity > @original_stock_location.count_on_hand(@variant)
-          raise InvalidQuantityError, Spree.t(:insufficient_stock_available)
-        end
+        require_usable_quantity_for_reserve(quantity)
         ActiveRecord::Base.transaction do
           reserved_stock_item = @user.reserved_stock_item_or_create(
             @variant,
@@ -32,12 +29,7 @@ module Spree
           )
           reserved_stock_item.stock_movements.create!(quantity: quantity)
           @original_stock_location.unstock(@variant, quantity)
-
-          if Gem::Specification.find_all_by_name('solidus_product_assembly').any?
-            @variant.product.parts.each do |part|
-              Spree::Stock::Reserver.new(part, @user, @original_stock_location, @reserved_stock_location).reserve(quantity, expires_at)
-            end
-          end
+          reserve_parts(quantity, expires_at) if using_solidus_product_assembly?
           reserved_stock_item
         end
       end
@@ -53,24 +45,10 @@ module Spree
           Spree.t(:no_stock_reserved_for_user_and_variant)
         ) unless reserved_stock_item.present?
         reserved_count_on_hand = reserved_stock_item.count_on_hand
-        if quantity.present?
-          if quantity < 1
-            raise InvalidQuantityError,
-                  Spree.t(:quantity_must_be_positive)
-          end
-          if quantity > reserved_count_on_hand
-            raise InvalidQuantityError,
-                  Spree.t(:insufficient_reserved_stock_available)
-          end
-        end
+        require_usable_quantity_for_restock(quantity, reserved_count_on_hand)
         quantity ||= reserved_count_on_hand
         perform_restock(reserved_stock_item, quantity)
-
-        if Gem::Specification.find_all_by_name('solidus_product_assembly').any?
-          @variant.product.parts.each do |part|
-            Spree::Stock::Reserver.new(part, @user, @original_stock_location, @reserved_stock_location).restock(quantity)
-          end
-        end
+        restock_parts(quantity) if using_solidus_product_assembly?
         reserved_stock_item
       end
 
@@ -87,11 +65,55 @@ module Spree
         variant = reserved_stock_item.variant
         user = reserved_stock_item.user
         original_stock_location = reserved_stock_item.original_stock_location
-        @reserved_stock_location.unstock(variant, quantity, nil, user, original_stock_location)
+        @reserved_stock_location.unstock(
+          variant, quantity, nil, user, original_stock_location
+        )
         original_stock_location.move(variant, quantity)
         reserved_stock_item.reload
         reserved_stock_item.destroy if reserved_stock_item.count_on_hand == 0
         reserved_stock_item
+      end
+
+      def reserve_parts(quantity, expires_at)
+        @variant.product.parts.each do |part|
+          Spree::Stock::Reserver.new(
+            part, @user, @original_stock_location, @reserved_stock_location
+          ).reserve(quantity, expires_at)
+        end
+      end
+
+      def restock_parts(quantity)
+        @variant.product.parts.each do |part|
+          Spree::Stock::Reserver.new(
+            part, @user, @original_stock_location, @reserved_stock_location
+          ).restock(quantity)
+        end
+      end
+
+      def require_usable_quantity_for_reserve(quantity)
+        if quantity < 1 || quantity.blank?
+          raise InvalidQuantityError, Spree.t(:quantity_must_be_positive)
+        end
+        if quantity > @original_stock_location.count_on_hand(@variant)
+          raise InvalidQuantityError, Spree.t(:insufficient_stock_available)
+        end
+      end
+
+      def require_usable_quantity_for_restock(quantity, reserved_count_on_hand)
+        if quantity.present?
+          if quantity < 1
+            raise InvalidQuantityError,
+                  Spree.t(:quantity_must_be_positive)
+          end
+          if quantity > reserved_count_on_hand
+            raise InvalidQuantityError,
+                  Spree.t(:insufficient_reserved_stock_available)
+          end
+        end
+      end
+
+      def using_solidus_product_assembly?
+        Gem::Specification.find_all_by_name("solidus_product_assembly").any?
       end
     end
   end
